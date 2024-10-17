6889ccd1dd0c28ee13d626ef02f23b713878f51a
shovelend <shovelend@gmail.com>
2024-10-17 12:36:09 +0100

    SPAN-3404 :bug: Fix dependency cycles and ordering when adding apt::source

--- manifests/repository/debian.pp
+++ manifests/repository/debian.pp
@@ -7,9 +7,8 @@ class confluent::repository::debian (
 
   apt::key { $apt_key:
     source => $key_url,
-    tag    => '__confluent__'
-  } ->
-  apt::source { 'confluent':
+  }
+  -> apt::source { 'confluent':
     comment  => 'Confluent repo',
     location => $repository_url,
     release  => 'stable',
@@ -18,12 +17,7 @@ class confluent::repository::debian (
       'src' => false,
       'deb' => true,
     },
-    tag      => '__confluent__'
-  } ~>
-  exec { 'apt-get update':
-    refreshonly => true,
   }
 
-  # The following line is disabled, since it caused a dependency cycle on puppet version 7
-  # Apt::Key<| tag == '__confluent__' |> -> Apt::Source<| tag == '__confluent__' |> -> Package<| tag == '__confluent__' |>
+  Apt::Key<| |> -> Apt::Source<| |> -> Package<| tag == '__confluent__' |>
 }
